a923a26f7f1b17cba8acd88029cfc557b6daf87b
Olek Myron <myrondell@gmail.com>
2021-06-30 22:54:44 +0300

    imported files

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+// refs
+export default {
+  features: document.querySelector('.features'),
+  work: document.querySelector('.work'),
+  team: document.querySelector('.team'),
+  clients: document.querySelector('.clients'),
+};
---
+++ src/js/studio.js
@@ -0,0 +1,25 @@
+import refs from './refs';
+import featuresTpl from '../templates/studio-tpl/features.hbs';
+import featuresData from '../data/studio-data/features.json';
+import workTpl from '../templates/studio-tpl/work.hbs';
+import workData from '../data/studio-data/work.json';
+import teamTpl from '../templates/studio-tpl/team.hbs';
+import teamData from '../data/studio-data/team.json';
+import clientsTpl from '../templates/studio-tpl/clients.hbs';
+import clientsData from '../data/studio-data/clients.json';
+
+// features section
+setSection(refs.features, featuresTpl, featuresData);
+
+// work section
+setSection(refs.work, workTpl, workData);
+
+// team
+setSection(refs.team, teamTpl, teamData);
+
+// clients
+setSection(refs.clients, clientsTpl, clientsData);
+
+function setSection(ref, template, data) {
+  ref.innerHTML = template(data);
+}
